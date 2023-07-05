6f117ad6ef0f2dd7ab4baac907d46167a1ae2d00
clarine <antibaclarine.com>
2023-07-05 08:50:21 +0300

    first commit

---
+++ src/Introduction/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.css';
+import { Fa500Px } from "react-icons/fa";
+
+
+
+
+const Introduction=()=>{
+    return(
+        <div>
+       <h1 className="heading">What I learnt in React</h1>
+        <p>React is a Javascript library</p>
+        <Fa500Px/>
+        < img src="https://res.cloudinary.com/dxkzrog52/image/upload/v1685637180/cld-sample-5.jpg" className="img"/>
+        </div>
+       
+    )
+}
+export default Introduction
